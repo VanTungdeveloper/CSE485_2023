fbf1bd0f5bc2b94f3b8ceb7fafa4a30c452b4df9
Bui Van Phu <buivanphu52@gmail.com>
2023-11-30 01:06:45 +0700

    phần của phú commit

---
+++ btth01_template/btth01/admin/article/author/edit_author.php
@@ -0,0 +1,56 @@
+<?php
+    include '../layout/header.php';
+
+    $id = empty($_GET['id'])?$_POST['id'] : $_GET['id'];
+    $sql = "select * from tacgia where ma_tgia=$id";
+    try {
+        $statement = $connect->prepare($sql);
+        $statement->execute();
+        $author = $statement->fetch();
+//        header("Location: category.php");
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+?>
+
+
+    <main class="container mt-5 mb-5">
+        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+        <div class="row">
+            <div class="col-sm">
+                <h3 class="text-center text-uppercase fw-bold">Sửa thông tin tác giả</h3>
+                <form action="edit_author.php" method="post">
+                <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatId">Mã thể loại</span>
+                        <input type="text" value="<?=$author['ma_tgia']?>" class="form-control" name="id" readonly>
+                    </div>
+
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tên thể loại</span>
+                        <input type="text"  value="<?=$author['ten_tgia']?>"class="form-control" name="name">
+                    </div>
+
+                    <div class="form-group  float-end ">
+                        <input name="submit" type="submit" value="Lưu lại" class="btn btn-success">
+                        <a href="author.php" class="btn btn-warning ">Quay lại</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
+
+<?php
+
+if (isset($_POST['submit'])) {
+    try {
+        $name_update = $_POST['name'];
+        $statement = $connect->prepare("UPDATE tacgia SET ten_tgia='$name_update' WHERE ma_tgia=?;");
+        $statement->execute([$id]);
+        header('location: author.php');
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
+
+<?php include '../layout/footer.php';?>
