0d25fcdf609229d3fbb90b3efa5a3562ce6cd5e3
kienkonn <nguyendinhkientlu@gmail.com>
2023-11-30 01:55:51 +0700

    kiênkondz commit

--- btth01_template/btth01/admin/article/add_article.php
+++ btth01_template/btth01/admin/article/add_article.php
@@ -1,33 +1,55 @@
 <?php
     require '../layout/header.php';
+    if ($connect != null) {
+        try {
+            $statement = $connect->prepare("select * from theloai");
+            $statement->execute();
+            $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $types= $statement->fetchAll();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            $connect = null;
+        }
+    }
 
-    $id = $name = '';
-    $id_error = $name_error =  '';
+    if ($connect != null) {
+        try {
+            $statement = $connect->prepare("select * from tacgia");
+            $statement->execute();
+            $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+            $authors= $statement->fetchAll();
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+            $connect = null;
+        }
+    }
 
+    $id = $name = $title= $typeInput= $tomtat = $author= $publish ='';
 
     if (isset($_POST['submit'])) {
-        if (empty($_POST['id'])) {
-            $id_error = 'Mã tác giả là bắt buộc';
-        } else {
-            $id = htmlspecialchars($_POST['id']);
-        }
-
-        if (empty($_POST['name'])) {
-            $name_error = 'Tên tác giả là bắt buộc';
-        } else {
-            $name = htmlspecialchars($_POST['name']);
-        }
-
+        $id = htmlspecialchars($_POST['id']);
+        $name = htmlspecialchars($_POST['name']);
+        $title = htmlspecialchars($_POST['title']);
+        $typeInput = htmlspecialchars($_POST['type']);
+        $tomtat = htmlspecialchars($_POST['tomtat']);
+        $author = htmlspecialchars($_POST['author']);
+        $publish = htmlspecialchars($_POST['publish']);
 
         if (empty($name_error)) {
-            $sql = "INSERT INTO theloai(ma_tloai, ten_tloai) VALUES (?, ?)";
+            $sql = "INSERT INTO baiviet(ma_bviet, tieude, ten_bhat, ma_tloai, tomtat, ma_tgia, ngayviet) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)";
             try {
                 $statement = $connect->prepare($sql);
                 $statement->bindParam(1, $id);
-                $statement->bindParam(2, $name);
+                $statement->bindParam(2, $title);
+                $statement->bindParam(3, $name);
+                $statement->bindParam(4, $typeInput);
+                $statement->bindParam(5, $tomtat);
+                $statement->bindParam(6, $author);
+                $statement->bindParam(7, $publish);
                 $statement->execute();
 
-                header("Location: author.php");
+                header("Location: article.php");
             } catch (PDOException $e) {
                 echo "Error: " . $e->getMessage();
             }
@@ -35,28 +57,54 @@
     }
 ?>
     <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
         <div class="row">
             <div class="col-sm">
-                <h3 class="text-center text-uppercase fw-bold">Thêm mới tác giả</h3>
-                <form action="add_author.php" method="post">
+                <h3 class="text-center text-uppercase fw-bold">Thêm bài viết</h3>
+                <form action="add_article.php" method="post">
                     <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatName">ID tác giả</span>
+                        <span class="input-group-text" id="lblCatName">ID bài viết</span>
                         <input type="text" class="form-control" name="id" >
                     </div>
-                        <p class="text-danger">
-                            <?php echo $id_error; ?>
-                        </p>
                     <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatName">Tên tác giả</span>
+                        <span class="input-group-text" id="lblCatName">Tiêu đề</span>
+                        <input type="text" class="form-control" name="title" >
+                    </div>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tên bài viết</span>
                         <input type="text" class="form-control" name="name" >
                     </div>
-                    <p class="text-danger">
-                        <?php echo $name_error; ?>
-                    </p>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Thể loại</span>
+                        <select name="type" class="form-select" aria-label="Default select example">
+                            <option value="" selected >Vui lòng chọn thể loại</option>
+                            <?php foreach ($types as $type): ?>
+                                <option value="<?= $type['ma_tloai']; ?>"><?= $type['ten_tloai']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tóm tắt</span>
+                        <textarea name="tomtat" rows="4" cols="50">
+
+                        </textarea>
+                    </div>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Tác giả</span>
+                        <select name="author" class="form-select" aria-label="Default select example">
+                            <option value="" selected >Vui lòng chọn tác giả</option>
+                            <?php foreach ($authors as $author): ?>
+                                <option value="<?= $author['ma_tgia']; ?>"><?= $author['ten_tgia']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="input-group mt-3 mb-3">
+                        <span class="input-group-text" id="lblCatName">Ngày viết</span>
+                        <input type="datetime-local" class="form-control" name="publish" >
+                    </div>
+
                     <div class="form-group  float-end ">
                         <input  name="submit" type="submit" value="Thêm" class="btn btn-success">
-                        <a href="http://localhost/CSE485_2023/btth01_template/btth01/admin/author/author.php" class="btn btn-warning ">Quay lại</a>
+                        <a href="http://localhost/CSE485_2023/btth01_template/btth01/admin/article/article.php" class="btn btn-warning ">Quay lại</a>
                     </div>
                 </form>
             </div>
--- btth01_template/btth01/admin/article/edit_article.php
+++ btth01_template/btth01/admin/article/edit_article.php
@@ -1,52 +1,138 @@
 <?php
-    include '../layout/header.php';
+include '../layout/header.php';
 
-    $id = empty($_GET['id'])?$_POST['id']: $_GET['id'];
-    $sql = "select * from theloai where ma_tloai=$id";
+if ($connect != null) {
     try {
-        $statement = $connect->prepare($sql);
+        $statement = $connect->prepare("select * from theloai");
         $statement->execute();
-        $type = $statement->fetch();
-//        header("Location: category.php");
+        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $types= $statement->fetchAll();
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
+        $connect = null;
     }
+}
+
+if ($connect != null) {
+    try {
+        $statement = $connect->prepare("select * from tacgia");
+        $statement->execute();
+        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $authors= $statement->fetchAll();
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+        $connect = null;
+    }
+}
+
+$id = empty($_GET['id'])?$_POST['id']: $_GET['id'];
+$sql = "select * from baiviet where ma_bviet=$id";
+try {
+    $statement = $connect->prepare($sql);
+    $statement->execute();
+    $article = $statement->fetch();
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
 ?>
 
 
     <main class="container mt-5 mb-5">
-        <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
-        <div class="row">
-            <div class="col-sm">
-                <h3 class="text-center text-uppercase fw-bold">Sửa thông tin thể loại</h3>
-                <form action="edit_category.php" method="post">
-                <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatId">Mã thể loại</span>
-                        <input type="text" value="<?=$type['ma_tloai']?>" class="form-control" name="id" readonly>
-                    </div>
-
-                    <div class="input-group mt-3 mb-3">
-                        <span class="input-group-text" id="lblCatName">Tên thể loại</span>
-                        <input type="text"  value="<?=$type['ten_tloai']?>"class="form-control" name="name">
-                    </div>
-
-                    <div class="form-group  float-end ">
-                        <input name="submit" type="submit" value="Lưu lại" class="btn btn-success">
-                        <a href="category.php" class="btn btn-warning ">Quay lại</a>
-                    </div>
-                </form>
-            </div>
+    <!-- <h3 class="text-center text-uppercase mb-3 text-primary">CẢM NHẬN VỀ BÀI HÁT</h3> -->
+    <div class="row">
+    <div class="col-sm">
+    <h3 class="text-center text-uppercase fw-bold">Sửa thông tin bài viết</h3>
+    <form action="edit_article.php" method="post">
+    <div class="input-group mt-3 mb-3">
+        <span class="input-group-text" id="lblCatName">ID bài viết</span>
+        <input type="text" class="form-control" name="id" readonly value="<?=$article['ma_bviet']?>">
+    </div>
+    <div class="input-group mt-3 mb-3">
+        <span class="input-group-text" id="lblCatName">Tiêu đề</span>
+        <input type="text" class="form-control" name="title" value="<?=$article['tieude']?>">
+    </div>
+    <div class="input-group mt-3 mb-3">
+        <span class="input-group-text" id="lblCatName">Tên bài viết</span>
+        <input type="text" class="form-control" name="name" value="<?=$article['ten_bhat']?>">
+    </div>
+    <div class="input-group mt-3 mb-3">
+    <span class="input-group-text" id="lblCatName">Thể loại</span>
+    <select name="type" class="form-select" aria-label="Default select example">
+        <option value="<?php echo $article['ma_tloai']?>" selected >
+        <?php
+        $name = $article['ma_tloai'];
+        $sql2 = 'select * from theloai where ma_tloai=?';
+        $statement = $connect->prepare($sql2);
+        $statement->execute([$name]);
+        $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $type = $statement->fetch();
+        echo $type['ten_tloai'];
+        ?>
+        </option>
+        <?php foreach ($types as $type): ?>
+            <option value="<?= $type['ma_tloai']; ?>"><?= $type['ten_tloai']; ?></option>
+        <?php endforeach; ?>
+    </select>
+    </div>
+        <div class="input-group mt-3 mb-3">
+            <span class="input-group-text" id="lblCatName">Tóm tắt</span>
+            <textarea name="tomtat" rows="4" cols="50">
+                            <?=$article['tomtat']?>
+                        </textarea>
         </div>
+        <div class="input-group mt-3 mb-3">
+            <span class="input-group-text" id="lblCatName">Tác giả</span>
+            <select name="author" class="form-select" aria-label="Default select example">
+                <option value="<?$article['ma_tgia']?>" selected >
+                    <?php
+                    $au = $article['ma_tgia'];
+                    $sql2 = 'select * from tacgia where ma_tgia=?';
+                    $statement = $connect->prepare($sql2);
+                    $statement->execute([$au]);
+                    $result = $statement->setFetchMode(PDO::FETCH_ASSOC);
+                    $aut = $statement->fetch();
+                    echo $aut['ten_tgia'];
+                    ?>
+                </option>
+                <?php foreach ($authors as $author): ?>
+                    <option value="<?= $author['ma_tgia']; ?>"><?= $author['ten_tgia']; ?></option>
+                <?php endforeach; ?>
+            </select>
+        </div>
+        <div class="input-group mt-3 mb-3">
+            <span class="input-group-text" id="lblCatName">Ngày viết</span>
+            <input type="datetime-local" class="form-control" name="publish" value="<?=$article['ngayviet']?>">
+        </div>
+
+        <div class="form-group  float-end ">
+            <input  name="submit" type="submit" value="Lưu" class="btn btn-success">
+            <a href="http://localhost/CSE485_2023/btth01_template/btth01/admin/article/article.php" class="btn btn-warning ">Quay lại</a>
+        </div>
+    </form>
+    </div>
+    </div>
     </main>
 
 <?php
 
 if (isset($_POST['submit'])) {
     try {
-        $name_update = $_POST['name'];
-        $statement = $connect->prepare("UPDATE theloai SET ten_tloai='$name_update' WHERE ma_tloai=?;");
+        $name_up = htmlspecialchars($_POST['name']);
+        $title_up = htmlspecialchars($_POST['title']);
+        $typeInput_up = htmlspecialchars($_POST['type']);
+        $tomtat_up = htmlspecialchars($_POST['tomtat']);
+        $author_up = htmlspecialchars($_POST['author']);
+        $publish_up = htmlspecialchars($_POST['publish']);
+        $statement = $connect->prepare("UPDATE baiviet 
+                SET ten_bhat='$name_up' ,
+                    tieude='$title_up',
+                    ma_tloai=$typeInput_up,
+                    tomtat='$tomtat_up',
+                    ma_tgia=$author_up,
+                    ngayviet='$publish_up'
+               WHERE ma_bviet=?;");
         $statement->execute([$id]);
-        header('location: category.php');
+        header('location: article.php');
     } catch (PDOException $e) {
         echo "Error: " . $e->getMessage();
     }
